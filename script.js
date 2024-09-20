da4656162a96180eaefbfa48b2c9ae4b56cf8083
Dhanush51 <kotegarkumbledhanush@gmail.com>
2024-09-20 21:20:51 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,27 @@
+function appendSymbol(symbol){
+    document.getElementById('display').value+=symbol;
+}
+
+function clearDisplay(){
+    document.getElementById('display').value="";
+}
+
+function deleteLast(){
+    let display=document.getElementById('display');
+    display.value=display.value.slice(0,-1);
+    
+}
+
+function calculate(){
+    let display=document.getElementById('display');
+    try{
+        display.value=eval(display.value);
+    }
+
+    catch{
+        display.value="Error";
+    }
+}
+
+
+
